a47db981f6ca9ce0f9cef6503e6881b041b65fbb
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-28 12:41:13 -0700

    wrote todo item render function

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,19 @@
 export function renderTodo(todo) {
     // create a div and a p tag
+    const div = document.createElement('div');
+    const todoEl = document.createElement('p');
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
-
+    if (todo.complete) {
+        div.classList.add('complete');
+    } else {
+        div.classList.add('incomplete');
+    }
     // add the 'todo' css class no matter what
-
+    div.classList.add('todo');
     // put the todo's text into the p tag
-
+    todoEl.textContent = todo.todo;
     // append stuff
-
+    div.append(todoEl);
     // return the div
+    return div;
 }
