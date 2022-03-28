53f361efc37878c67b52563c89c9d6e4a91150be
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-28 16:39:35 -0700

    added delete todo button to completed todos

--- render-utils.js
+++ render-utils.js
@@ -2,18 +2,21 @@ export function renderTodo(todo) {
     // create a div and a p tag
     const div = document.createElement('div');
     const todoEl = document.createElement('p');
-    // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
-    if (todo.complete) {
-        div.classList.add('complete');
-    } else {
-        div.classList.add('incomplete');
-    }
-    // add the 'todo' css class no matter what
-    div.classList.add('todo');
+    const removeButton = document.createElement('button');
     // put the todo's text into the p tag
     todoEl.textContent = todo.todo;
+    // add the 'todo' css class no matter what
+    div.classList.add('todo');
     // append stuff
     div.append(todoEl);
     // return the div
+    // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
+    if (todo.complete) {
+        div.classList.add('complete');
+        removeButton.textContent = 'Delete todo';
+        div.append(removeButton);
+    } else {
+        div.classList.add('incomplete');
+    }
     return div;
 }
